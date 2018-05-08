9f8e07a7fb402b5a7855e867f01b5a1bf0fbd605
Steve Rittmeyer <steve@ustomp.com>
2018-05-08 15:27:06 -0400

    moved to artifacts model

--- js/projects.js
+++ js/projects.js
@@ -1,30 +1,17 @@
-angular.module('projectsApp', [])
-	.controller('ProjectsListController', function() {
+var projectsApp = angular.module('projectsApp', []);
 
-		var projectList = this;
+projectsApp.controller('ProjectsCtrl', function($scope, projectRetriever) {
 
-		projectList.projects = [
-			{name:'Ampeg Scrambler', shortName:'scrambler'},
-			{name:'Boss FA-1', shortName:'fa1'},
-			{name:'TS-808', shortName:'808'},
-			{name:'Boss SD-1', shortName:'sd1'},
-			{name:'Dr Quack', shortName:'drquack'},
-			{name:'Dual Orange Squeezer', shortName:'dualsqueeze'},
-			{name:'Kay Fuzztone', shortName:'kayfuzz'},
-			{name:'MXR Dyna-Comp', shortName:'dynacomp'},
-			{name:'Maestro FSH-1A', shortName:'fsh'},
-			{name:'PT-80 Delay', shortName:'pt80'},
-			{name:'Peppermill', shortName:'peppermill'},
-			{name:'Rat', shortName:'rat'},
-			{name:'Shin-Ei Fuzz', shortName:'shinei'},
-			{name:'Unicord Super-Fuzz', shortName:'superfuzz'}
-		];
+		projectRetriever.getProjects().then(function(projects) {
 
+			$scope.projects = projects;
+		});
 
-	})
-	.filter('urlBuilder', function() {
+});
 
-		return function(project, type, extension, format) {
+projectsApp.filter('urlBuilder', function() {
+
+		return function(project, artifact, format) {
 
 			var githubFormat = format || 'blob';
 
@@ -32,13 +19,36 @@ angular.module('projectsApp', [])
 
 			var url = urlPrefix + project.name + '/';
 
-			if(type && extension) {
+			if(artifact) {
 
-				url = url + project.shortName + type + '.' + extension;
+				url = url + project.shortName + artifact;
 			}
 
 			return url; 
 		};
-	});
+});
+
+projectsApp.service('projectRetriever', function($http, $q) {
+
+		var deferred = $q.defer();
+
+
+		$http.get('projects.json').then(function(response) {
+		
+			deferred.resolve(response.data);
+
+		}).catch(function(response) {
+		
+			console.error(response);
+
+			deferred.resolve([]);
+		});
+
+		this.getProjects = function() {
+
+			return deferred.promise;
+		}
+	
+});
 
 
